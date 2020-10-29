7b3d554d2b60125eb6573da36a94871f72a08f07
ashlinhanson <ashlinhanson@gmail.com>
2020-10-28 20:29:00 -0500

    all modals work and added

--- public/parks.js
+++ public/parks.js
@@ -8,6 +8,36 @@ $(document).ready(function () {
     //     modal.removeClass("invisible").addClass("visible");
     //   }
       
+    function activitiesModal(){
+        let button = $('#activitiesBtn');
+
+        button.on("click", function(event){
+            event.preventDefault();
+
+            $('#modal1').modal.show();
+        })
+    }
+    
+    function activitiesModal(){
+        let button = $('#amenitiesBtn');
+
+        button.on("click", function(event){
+            event.preventDefault();
+
+            $('#modal2').modal.show();
+        })
+    }
+
+    function regulationsModal(){
+        let button = $('#regulationsBtn');
+
+        button.on("click", function(event){
+            event.preventDefault();
+
+            $('#modal3').modal.show();
+        })
+    }
+
     function reservationModal(){
         let button = $('#resBtn');
 
