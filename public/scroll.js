e9b1c59f4cd0ebf5fbf02743703777a50e82b4b3
naywilkins512 <nathanwilkins01@gmail.com>
2020-10-30 01:11:55 -0500

    adds footer to parks html adds sizing to index.html

--- public/scroll.js
+++ public/scroll.js
@@ -1,40 +1,67 @@
 gsap.registerPlugin(ScrollTrigger);
 
+gsap.to("#seventh", {
+    scrollTrigger: {
+        trigger: "top", 
+        scrub: 0.5,
+        pin: true  
+    },
+    y: 100,
+    x: 100, 
+    duration: 6, 
+});
+gsap.to("#sixth", {
+    scrollTrigger: {
+        trigger: "top", 
+        scrub: 0.5,
+        pin: true  
+    },
+    y: 100,
+    x: -100, 
+    duration: 6, 
+});
 
 gsap.to("#fifth", {
     scrollTrigger: {
         trigger: "top", 
-        scrub: 0.5,  
+        scrub: 0.2,
+       
+         
     },
-    y: -900,  
+    y: 300, 
     duration: 6, 
 });
 
 gsap.to("#fourth", {
     scrollTrigger: {
         trigger: "top", 
-        scrub: 0.5,  
+        scrub: 0.2,  
     },
-    y: -700,  
+    y: 400, 
     duration: 6, 
 });
 
 gsap.to("#third", {
     scrollTrigger: {
-        trigger: "#fourth", 
-        start: "top bottom",
+        trigger: "top",
+        // start: top,
+        // end: bottom,
         scrub: 0.5,  
+        
     },
-    y: -700,  
+    y: 600, 
+    opacity: 0,  
     duration: 6, 
 });
 
 gsap.to("#second", {
     scrollTrigger: {
-        trigger: "#fourth", 
+        trigger: "top", 
         scrub: 0.5,  
+        
     },
-    y: -600,  
+    y: 600,  
+    opacity: 0, 
     duration: 6, 
 });
 
@@ -42,8 +69,10 @@ gsap.to("#first", {
     scrollTrigger: {
         trigger: "top", 
         scrub: 0.5,  
+        
     },
-    y: -500,  
+    y: 600,  
+    opacity: 0, 
     duration: 6, 
 });
 
@@ -52,7 +81,7 @@ gsap.to(".title", {
         trigger: "top", 
         scrub: 0.5,  
     },
-    y: -800,  
+    y: -100,  
     opacity: 0,
     duration: 1, 
 });
