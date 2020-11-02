3fac9d25c6a77311282ceda7dde022801e4e8ab3
Steve Rodriguez <smrodriguez88@gmail.com>
2020-11-01 18:02:48 -0600

    GROUP CODING SESSION: backend/frontend inegration, reservation input form working, data displayed on parks page, working MVP

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 const express = require('express');
 const cors = require('cors')
 const PORT = process.env.PORT || 8080;
-
 const app = express();
-
 var db = require("./models");
 
-var corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200
-}
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
 // let routes = require("./routes/api-routes");
 
 // app.use(routes);
@@ -23,7 +14,8 @@ app.use(function(req, res, next) {
 require("./routes/api-routes.js")(app);
 
 db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
+    require("./controllers/parks_controller.js");
+    app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 });
